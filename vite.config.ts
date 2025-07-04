e16c7c1e2fd9e684d087f42d5a6b6a1dbce32f73
Jasper Chen <155693124+Jasperc2024@users.noreply.github.com>
2025-07-04 13:45:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Domapus/",
+  base: '/Domapus/',
   server: {
     host: "::",
     port: 8080,
