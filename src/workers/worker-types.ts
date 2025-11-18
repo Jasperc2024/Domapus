fccdcdda60090d42923ea71c5504e66b483e5053
Jasper Chen <jasperchen925@gmail.com>
2025-11-17 21:10:30 -0500

    Optimize data worker

--- src/workers/worker-types.ts
+++ src/workers/worker-types.ts
@@ -18,10 +18,18 @@ export interface LoadDataRequest {
   selectedMetric: string;
 }
 
+export interface Viewport {
+  minX: number; // west
+  minY: number; // south
+  maxX: number; // east
+  maxY: number; // north
+}
+
 export interface ProcessGeoJSONRequest {
   geojson: GeoJSON.FeatureCollection;
   zipData: Record<string, ZipData>;
   selectedMetric: string;
+  viewport: Viewport | null;
 }
 
 export interface DataProcessedResponse {
